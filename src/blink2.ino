c2982983721250dcc33b3434084dcf9778b7d04a
Alexander <aalexanderfc@gmail.com>
2024-03-13 10:31:39 +0100

    sist

--- src/blink2.ino
+++ src/blink2.ino
@@ -1,23 +1,29 @@
-const int buttonPin1 = 2; //GPIO1
-const int buttonPin2 = 3; //GPIO2
+const int buttonPin1 = 2; // GPIO1
+const int buttonPin2 = 3; // GPIO2
 
-const int ledPinR = 13; //Diod-R
-const int ledPinG = 12; //Diod-G
-const int ledPinB = 11; //Diod-B
+const int ledPinR = 13; // Diod-R
+const int ledPinG = 12; // Diod-G
+const int ledPinB = 11; // Diod-B
 
 int potentioMeter = A0;
+
+bool button1Enabled = true; // Variabel för att hålla koll på om knapp 1 är aktiverad
+bool button2Enabled = true; // Variabel för att hålla koll på om knapp 2 är aktiverad
 
 bool buttonState1 = false;
 bool lastButtonState1 = false;
 unsigned long lastDebounceTime1 = 0;
+unsigned long debounceDelay1 = 50; // Debounce-fördröjning i millisekunder
 
 bool buttonState2 = false;
 bool lastButtonState2 = false;
 unsigned long lastDebounceTime2 = 0;
-
-unsigned long debounceDelay = 50;
+unsigned long debounceDelay2 = 50;
 
 int lastPotValue = 0;
+unsigned long lastPotReadTime = 0;
+unsigned long potDebounceDelay = 50; // Debounce-fördröjning för potentiometern i millisekunder
+
 bool potentiometerEnabled = false;
 
 void setup() {
@@ -28,6 +34,9 @@ void setup() {
   pinMode(ledPinR, OUTPUT);
   pinMode(ledPinG, OUTPUT);
   pinMode(ledPinB, OUTPUT);
+
+  attachInterrupt(digitalPinToInterrupt(buttonPin1), handleButton1Interrupt, FALLING);
+  attachInterrupt(digitalPinToInterrupt(buttonPin2), handleButton2Interrupt, FALLING);
 }
 
 void loop() {
@@ -36,9 +45,9 @@ void loop() {
     executeCommand(command);
   }
 
-  if (potentiometerEnabled) {
+  if (potentiometerEnabled && (millis() - lastPotReadTime >= potDebounceDelay)) {
     int val = analogRead(potentioMeter);
-    val = map(val, 0, 1023, 0, 255); // Kartlägg potentiometeravläsningen till intervallet för PWM
+    val = map(val, 0, 1023, 0, 255); // Kartlägg potentiometeravläsningen till ett värde mellan 0 och 255
 
     if (val != lastPotValue) {
       analogWrite(ledPinB, val);
@@ -46,70 +55,73 @@ void loop() {
       Serial.println(val);
       lastPotValue = val;
     }
+    lastPotReadTime = millis();
   }
 
-  int reading1 = digitalRead(buttonPin1);//för att läsa tillståndet av knapp 1
-  if (reading1 != lastButtonState1) {
+  delay(10); // Låt en kort paus för att undvika flera avbrott
+}
+
+void handleButton1Interrupt() {
+  if ((millis() - lastDebounceTime1) > debounceDelay1) {
+    if (button1Enabled) {
+      buttonState1 = !buttonState1;
+      digitalWrite(ledPinR, buttonState1 ? HIGH : LOW);
+    }
     lastDebounceTime1 = millis();
   }
+}
 
-  if ((millis() - lastDebounceTime1) > debounceDelay) {
-    if (reading1 != buttonState1) {
-      buttonState1 = reading1;
-
-      if (buttonState1 == LOW) {
-        digitalWrite(ledPinR, !digitalRead(ledPinR));
-      }
+void handleButton2Interrupt() {
+  if ((millis() - lastDebounceTime2) > debounceDelay2) {
+    if (button2Enabled) {
+      buttonState2 = !buttonState2;
+      digitalWrite(ledPinG, buttonState2 ? HIGH : LOW);
     }
-  }
-
-  lastButtonState1 = reading1;//för att lagra det föregående tillståndet för knapp
-
-  int reading2 = digitalRead(buttonPin2);//för att läsa tillståndet av knapp 2
-  if (reading2 != lastButtonState2) {
     lastDebounceTime2 = millis();
   }
-
-  if ((millis() - lastDebounceTime2) > debounceDelay) {
-    if (reading2 != buttonState2) {
-      buttonState2 = reading2;
-
-      if (buttonState2 == LOW) {
-        digitalWrite(ledPinG, !digitalRead(ledPinG));
-      }
-    }
-  }
-
-  lastButtonState2 = reading2;//för att lagra det föregående tillståndet för knapp
-
-  delay(100);
 }
 
 void executeCommand(String command) {
   if (command.startsWith("enable button 1")) {
-    pinMode(buttonPin1, INPUT_PULLUP);
+    button1Enabled = true;
     Serial.println("Button 1 enabled.");
   } else if (command.startsWith("disable button 1")) {
-    pinMode(buttonPin1, INPUT);
+    button1Enabled = false;
     Serial.println("Button 1 disabled.");
   } else if (command.startsWith("enable button 2")) {
-    pinMode(buttonPin2, INPUT_PULLUP);
+    button2Enabled = true;
     Serial.println("Button 2 enabled.");
   } else if (command.startsWith("disable button 2")) {
-    pinMode(buttonPin2, INPUT);
+    button2Enabled = false;
     Serial.println("Button 2 disabled.");
   } else if (command.startsWith("ledon 1")) {
-    digitalWrite(ledPinR, HIGH);
-    Serial.println("LED 1 turned on.");
+    if (button1Enabled) {
+      digitalWrite(ledPinR, HIGH);
+      Serial.println("LED 1 turned on.");
+    } else {
+      Serial.println("Button 1 is disabled.");
+    }
   } else if (command.startsWith("ledoff 1")) {
-    digitalWrite(ledPinR, LOW);
-    Serial.println("LED 1 turned off.");
+    if (button1Enabled) {
+      digitalWrite(ledPinR, LOW);
+      Serial.println("LED 1 turned off.");
+    } else {
+      Serial.println("Button 1 is disabled.");
+    }
   } else if (command.startsWith("ledon 2")) {
-    digitalWrite(ledPinG, HIGH);
-    Serial.println("LED 2 turned on.");
+    if (button2Enabled) {
+      digitalWrite(ledPinG, HIGH);
+      Serial.println("LED 2 turned on.");
+    } else {
+      Serial.println("Button 2 is disabled.");
+    }
   } else if (command.startsWith("ledoff 2")) {
-    digitalWrite(ledPinG, LOW);
-    Serial.println("LED 2 turned off.");
+    if (button2Enabled) {
+      digitalWrite(ledPinG, LOW);
+      Serial.println("LED 2 turned off.");
+    } else {
+      Serial.println("Button 2 is disabled.");
+    }
   } else if (command.startsWith("ledpower ")) {
     int powerValue = command.substring(9).toInt();
     if (powerValue == -1) {
@@ -122,4 +134,3 @@ void executeCommand(String command) {
     }
   }
 }
-
